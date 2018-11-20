1f4a73eb849a03533b22a1a279da579006a6077d
Arainzhe <wangzhe1810@163.com>
2018-11-20 16:53:05 +0800

    Blog.Core第四天

--- Blog.Core.Repository/sugar/BaseDBConfig.cs
+++ Blog.Core.Repository/sugar/BaseDBConfig.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Blog.Core.Repository.sugar
 {
     public class BaseDBConfig
     {
-
+        public static string ConnectionString = File.ReadAllLines(@"I:\Myproject\NotCore\Blog.Core.Repository\Db.txt").ToString().Trim();
     }
 }
--- Blog.Core.Repository/sugar/DbContext.cs
+++ Blog.Core.Repository/sugar/DbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,109 @@ namespace Blog.Core.Repository.sugar
 {
     public class DbContext
     {
+        private static string _connectionString;
+        private static DbType _dbType;
+        private SqlSugar.SqlSugarClient _db;
 
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public static string ConnectionString
+        {
+            get { return _connectionString; }
+            set { _connectionString = value; }
+        }
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public static DbType dbType
+        {
+            get { return _dbType; }
+            set { _dbType = value; }
+        }
+        /// <summary>
+        /// 数据库连接对象
+        /// </summary>
+        public SqlSugar.SqlSugarClient Db
+        {
+            get { return _db; }
+            set { _db=value; }
+        }
+        public static DbContext Context
+        {
+            get { return new DbContext(); }
+        }
+        /// <summary>
+        /// 功能函数：构造函数
+        /// </summary>
+        private DbContext()
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+                throw new ArgumentNullException("数据库连接字符串为空");
+            _db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = _connectionString,
+                DbType = _dbType,
+                IsAutoCloseConnection = true,
+                IsShardSameThread = true,
+                ConfigureExternalServices = new ConfigureExternalServices()
+                {
+                    //DataInfoCacheService = new HttpRuntimeCache()
+                },
+                MoreSettings = new ConnMoreSettings()
+                {
+                    IsAutoRemoveDataCache = true
+                },
+            });
+        }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="blnIsAutoCloseConnection">是否自动关闭连接</param>
+        private DbContext(bool blnIsAutoCloseConnection)
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+                throw new ArgumentNullException("数据库连接字符串为空");
+            _db = new SqlSugarClient(new ConnectionConfig()
+            {
+               ConnectionString = _connectionString,
+               DbType=_dbType,
+               IsAutoCloseConnection=blnIsAutoCloseConnection,
+               IsShardSameThread=true,
+               ConfigureExternalServices=new ConfigureExternalServices()
+               {
+                   //DataInfoCacheService = new HttpRuntimeCache()
+               },
+               MoreSettings=new ConnMoreSettings()
+               {
+                   IsAutoRemoveDataCache=true,
+               }
+            });
+        }
+
+        #region 实例方法
+        /// <summary>
+        /// 功能描述：获取数据库处理对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public SimpleClient<T> GetEntityDB<T>() where T : class,new()
+        {
+            return new SimpleClient<T>(_db);
+        }
+        #endregion
+
+        #region 获取数据库处理对象
+        /// <summary>
+        /// 获取数据库处理对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public SimpleClient<T> GetEntityDB<T>(SqlSugarClient db) where T : class, new()
+        {
+            return new SimpleClient<T>(db);
+        }
+        #endregion
     }
 }
